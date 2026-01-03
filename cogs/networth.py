215977deca8b25087c51e480323252a782ac1a62
soundofhorizon <49669345+soundofhorizon@users.noreply.github.com>
2026-01-03 10:25:45 +0900

    [FIX] await

--- cogs/networth.py
+++ cogs/networth.py
@@ -167,5 +167,5 @@ class NetWorthCalc(commands.Cog):
             await ch.send(embed=embed)
 
 
-def setup(bot):
-    bot.add_cog(NetWorthCalc(bot))
+async def setup(bot):
+    await bot.add_cog(NetWorthCalc(bot))
